ab17be43d3d1c85c4947deaed857084218b09b7d
morelir <58606266+morelir@users.noreply.github.com>
2023-09-05 14:49:02 +0300

    fixed

--- client/src/BgRemove.js
+++ client/src/BgRemove.js
@@ -1,13 +1,17 @@
 import React from "react";
-import "./BgRemove.css"
+import close from "./assets/close.png";
+import "./BgRemove.css";
 
 const BigRemove = () => {
   return (
-    <div className="bg_div_cont">
-      <div className="bg_div_header">
-        <div className="bg_div_title">העלאת תמונה כדי להסיר את הרקע</div>
-        <button className="bg_header_button">העלאת תמונה</button>
-        <div>פורמטים נתמכים png, jpeg</div>
+    <div className="bg_cont">
+      <div className="bg_header">
+        <img src={close} className="close_img" />
+        <div className="bg_header_title">העלאת תמונה כדי להסיר את הרקע</div>
+        <div className="bg_header_second_subdiv">
+          <button className="bg_header_button">העלאת תמונה</button>
+          <div className="bg_header_subtext">png, jpeg :פורמטים נתמכים</div>
+        </div>
       </div>
     </div>
   );
